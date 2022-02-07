e3ec9bed3746902878e42bb6c15bb04585f2037b
Simple Dimple John <simpledimplejohn@gmail.com>
2022-02-07 11:49:41 -0800

    add binary tree examples

--- src/main/java/com/revature/binarytrees/NodeDriver.java
+++ src/main/java/com/revature/binarytrees/NodeDriver.java
@@ -7,7 +7,7 @@ public class NodeDriver {
 		// make a binary tree
 		Node newNode = new Node(20);
 		
-		System.out.println("root node: "+newNode.data);
+//		System.out.println("root node: "+newNode.data);
 		
 		newNode.add(4);
 		newNode.add(9);
@@ -18,10 +18,75 @@ public class NodeDriver {
 		newNode.add(30);
 		newNode.add(27);
 		
-		System.out.println(newNode.left.data);
+//		System.out.println(newNode.left.data);
 		
-		newNode.printAll();
-
+//		newNode.printAll();
+//		System.out.println("printing left side");
+//		printLeft(newNode);
+//		leftViewOfTree(newNode, 0); // needs level so it can pass it through
+		
+//		printAllNodesUnOrdred(newNode);
+//		printNodesOrdered(newNode);
+		printLeftOrdered(newNode);
+		
 	}
+	
+	// how to print just the left side of a node only only for loop
+	public static void printLeft(Node root) {
+		if(root == null) {
+			return;
+		}
+		for(Node node = root; node != null; node = node.left) {
+			System.out.println(node.data);
+		}
+	}
+	
+	// print left side only ordered
+	
+	public static void printLeftOrdered(Node node) {
+		if(node.left != null) {
+			printLeftOrdered(node.left);
+		}
+		System.out.println(node.data);
+		
+	}
+	
+	// recursivly print the left side of all left side nodes
+	static int maxLevel;
+	public static void leftViewOfTree(Node node, int level) {
+		if(node == null) {
+			return;
+		}
+		if(level >= maxLevel) {
+			System.out.println(node.data + " ");
+			maxLevel++;
+		}
+		leftViewOfTree(node.left, level + 1);
+		leftViewOfTree(node.right, level + 1);
+		
+	}
+	
+	public static void printAllNodesUnOrdred(Node node) {
+		if(node == null) {
+			return;
+		}
+		System.out.println(node.data);
+		printAllNodesUnOrdred(node.left);
+		printAllNodesUnOrdred(node.right);
+	}
+	
+	
+	public static void printNodesOrdered(Node node) {
+		if(node.left != null) {
+			printNodesOrdered(node.left);
+		}
+		System.out.println(node.data);
+		if(node.right != null) {
+			printNodesOrdered(node.right);
+		}
+	}
+	
+	
+	
 
 }
